550c956f525a580993d470392d0b3a309e019a44
Greg Bruno <gbruno@2ndwatch.com>
2016-10-31 22:16:14 -0700

    Adding Terraform templates. Enjoy

--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "global" {
   type = "map"
   default = {
    region = "us-east-1"
-   vpc_name = "my-sandbox"
-   internet-gateway-1 = "my-sandbox-igw"
+   vpc_name = "my-env"
+   internet-gateway-1 = "my-env-igw"
    public_subnet-1_az = "us-east-1a"
    private_subnet-1_az = "us-east-1a"
    public_subnet-1_name = "Public_subnet-1" 
@@ -11,9 +11,9 @@ variable "global" {
    vpc_cidr = "10.0.0.0/16"
    public-1_cidr = "10.0.1.0/24"
    private-1_cidr = "10.0.2.0/24"
-   owner = "OWNER"
-   pem_key_name = "Greg2wTest"
-   my_ip = "67.185.132.195/32"
+   owner = "your_name_here"
+   pem_key_name = "private_key_name"
+   my_ip = "12.34.56.78/32"
    ubuntu_ami = "ami-e3c3b8f4"
    windows_ami = "ami-3f0c4628"
   }
